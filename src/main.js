7a7789e641d09dc19afe2c8da2031aa8ad92abdf
sandeepkdasari143 <sandeepkumar25820@gmail.com>
2023-05-22 16:15:17 +0530

    Fetched All The Posts using IIFE

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
+const FETCH_URL = "https://jsonplaceholder.typicode.com/posts";
+(async () => {
+  const response = await fetch(FETCH_URL);
+  const jsonData = await response.json();
+  console.log(jsonData)
+  localStorage.setItem("blogs", JSON.stringify(jsonData))
+})();
 
-import { setupCounter } from './counter.js'
+// import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <button class="bg-red-500" id="counter" type="button"></button>
-  </div>
-`
+// document.querySelector('#app').innerHTML = `
+//   <div>
+//     <button class="bg-red-500" id="counter" type="button"></button>
+//   </div>
+// `
 
-setupCounter(document.querySelector('#counter'));
+// setupCounter(document.querySelector('#counter'));
