86ce5ec0f21598e1b7e91f44a664fccdb616df94
sandeepkdasari143 <sandeepkumar25820@gmail.com>
2023-05-22 16:55:49 +0530

    Rendered Blog Cards on Web Page with decent UI

--- src/main.js
+++ src/main.js
@@ -1,17 +1,36 @@
 const FETCH_URL = "https://jsonplaceholder.typicode.com/posts";
+let blogs = [];
+const blogsListingSection = document.querySelector("#blogs");
+
 (async () => {
   const response = await fetch(FETCH_URL);
   const jsonData = await response.json();
-  console.log(jsonData)
+  blogs = jsonData;
   localStorage.setItem("blogs", JSON.stringify(jsonData))
+  renderBlogs(blogs);
 })();
 
-// import { setupCounter } from './counter.js'
+function renderBlogs(blogs) {
+  blogsListingSection.setAttribute("class", "flex flex-wrap gap-3 items-center justify-center");
+  blogs.map((blog) => appendEachBlog(blog));
+}
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <button class="bg-red-500" id="counter" type="button"></button>
-//   </div>
-// `
+function appendEachBlog(blog) {
+  const eachBlog = document.createElement("article");
+  eachBlog.setAttribute("class", "w-[300px] border p-3 h-[350px] flex flex-col gap-3");
 
-// setupCounter(document.querySelector('#counter'));
+  eachBlog.innerHTML = `
+    <h1 class="font-bold text-2xl">${blog.title}</h1>
+    <p class="font-semibold line-clamp-3">${blog.body}</p>
+    <div class="flex flex-row gap-5">
+      <button class="px-5 py-2 border-none outline-none bg-green-500 rounded-md font-semibold text-white">
+        View Blog
+      </button>
+      <button class="px-5 py-2 border-none outline-none bg-red-500 rounded-md font-semibold text-white">
+        Delete Blog
+      </button>
+    </div>
+  `;
+
+  blogsListingSection.appendChild(eachBlog);
+}
